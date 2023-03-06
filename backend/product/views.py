15417851d07cc627dd52807abb8e1d7782f8f11f
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-06 18:15:56 -0400

    views file, testing data type response

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,7 +1,24 @@
 from django.http import JsonResponse
+import json 
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"messege":"Hi deandrade lovers"})
+    # request -> httpRequest -> Django
+    # print(dir(request))
+    # request.body
+    print(request.GET)
+
+    body = request.body # byte string of JSON data
+    print(body)
+
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
 
 
