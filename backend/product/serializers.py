e79c23c0660e9a1f40071fe8b33ced592e3af6fc
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-06 18:16:51 -0400

    serializer product field

---
+++ backend/product/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+# to retrive json data
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Product
+        fields = ( 
+            "id",
+            "name",
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail"
+        )
+        
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+    
+    class Meta:
+        
+        model = Category
+        fields = ( 
+            "id",
+            "name",
+            "get_absolute_url",
+            "products",
+        )
