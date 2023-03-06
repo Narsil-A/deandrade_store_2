2a0b868141c5bf3503f58a76d1285a95e7b55e39
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-06 18:12:39 -0400

    py_client eco get data

--- py_client/basic.py
+++ py_client/basic.py
@@ -8,10 +8,10 @@ endpoint = "http://localhost:8000/product"
 
 # requests.get() 
 
-get_response = requests.get(endpoint, json={"query":"Hello world"}) # HTTP request
+get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello world"}) # HTTP request
 
-print(get_response.json()['messege']) # print raw text response 
+print(get_response.json()) # print raw text response 
 
-print(get_response.text)
+# print(get_response.text)
 
 print(get_response.status_code)
