59b6047ee36a50b62e75487c34ea63e59599a797
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-09 16:59:45 -0400

    ingesting data with serializer

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,15 +1,16 @@
-from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+from product.serializers import ProductSerializer
 
-from .models import Product
-from .serializers import ProductSerializer
+@api_view(['POST'])
+def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    serializer = ProductSerializer(data=request.data) # send the data and verify if match with the requirements of the ProductSerializer.
+    if serializer.is_valid(raise_exception=True):  # validation for the front
+        print(serializer.data)
 
-
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    # lookup_field = 'pk' ??
-
-product_detail_view = ProductDetailAPIView.as_view()
-
-
+        return Response(serializer.data)
+    return Response({"invalid":"not good data"}, status=400)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,11 +4,11 @@ import requests
 
 # endpoint = "https://httpbin.org/anything"
 
-endpoint = "http://127.0.0.1:8000/product"
+endpoint = "http://127.0.0.1:8000/product/"
 
 # requests.get() 
 
-get_response = (requests.get(endpoint, json={"query":"Hello world"})) # HTTP request
+get_response = requests.post(endpoint, json={"id":2, "name":"creme"}) # HTTP request
 
 print(get_response.json()) # print raw text response 
 
