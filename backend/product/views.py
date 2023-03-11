a89179917d25307e9465e57134c5a43182fc8d08
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-10 23:34:43 -0400

    CreateApiView for category model id

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,25 +1,45 @@
+from django.http import Http404
+
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
 
 
-class ProductCreateAPIView(generics.CreateAPIView): # post method
-
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-
+class ProductCreateAPIView(generics.CreateAPIView): # post method create product objects
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        name = serializer.validated_data.get('name')
+        description = serializer.validated_data.get('description') or None
+        if description is None:
+            description = name
+        serializer.save(description=description)
+        # send a Django signal
 
 product_create_view = ProductCreateAPIView.as_view()
-
 class ProductDetailAPIView(generics.RetrieveAPIView): # get method
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk' primary key
 
 product_detail_view = ProductDetailAPIView.as_view()
+class CategoryDetailAPIView(generics.CreateAPIView):
+
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data) 
+
 
 
--- py_client/create.py
+++ py_client/create.py
@@ -3,9 +3,9 @@ import requests
 endpoint = "http://localhost:8000/product/" 
 
 data = {
-    "name",
-    "description",
-    "price",
+    "name":"facial creme",
+    "description":"creme for face",
+    "price":55,
 }
 get_response = requests.post(endpoint, json=data)
 print(get_response.json())
