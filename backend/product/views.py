f682fe5a8af7493efb2c9c29f58661f909f3eb70
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-14 11:18:38 -0400

    class ProductMixinView

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
+from django.shortcuts import get_object_or_404
 
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -47,3 +48,36 @@ class ProductListAPIView(generics.ListAPIView): # get method
     # lookup_field = 'pk' primary key
 
 product_list_view = ProductListAPIView.as_view() """ 
+
+# Update and Destroy method 
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.APIView
+):
+    # Get method
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs): # HTTP --> get
+        pk = kwargs.get('pk')
+
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    # Post method
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
+
--- py_client/create.py
+++ py_client/create.py
@@ -3,9 +3,9 @@ import requests
 endpoint = "http://localhost:8000/product/" 
 
 data = {
-    "name":"facial",
-    "description": "face",
-    "price":78,
+    "name":"Body creme",
+    "description": "body creme ",
+    "price":46,
 }
 get_response = requests.post(endpoint, json=data)
 print(get_response.json())
