dcdc2af1ab27e3d952415129be880126dd569952
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-20 23:22:28 -0400

    permissions.py, admin user permissions, permissions.DjangoModelPermissions

--- backend/product/views.py
+++ backend/product/views.py
@@ -5,7 +5,9 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Product
+from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer, CategorySerializer
+
 
 
 
@@ -19,7 +21,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         name = serializer.validated_data.get('name')
