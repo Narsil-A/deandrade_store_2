e7c835999eb54dfb3d076a9cdcf90aca15e2e443
andrea camargo <simbelmyne.evermind.131717@gmail.com>
2023-03-21 20:38:50 -0400

    py_client/list.py token authoritations

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 from . import views
 
 
 urlpatterns = [
+    path('auth/', obtain_auth_token), # creating an endpoint to generate the auth
     path('', views.product_list_create_view),
     path('<int:pk>/update/', views.product_update_view),
     path('<int:pk>/destroy/', views.product_destroy_view),
